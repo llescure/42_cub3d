cd1830867806069b3616f595c2f0c032a4718040
llescure <lea.lescure@hotmail.fr>
2021-03-10 17:16:22 +0100

    can display a blue square on a window + working on exiting by clicking on the red cross

--- minilibx/minilibx.h
+++ minilibx/minilibx.h
@@ -13,17 +13,12 @@
 #ifndef MINILIBX_H
 # define MINILIBX_H
 
-#include <mlx.h>
+#include "minilibx-linux/mlx.h"
 #include <X11/keysym.h>
 #include <X11/X.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-
-typedef struct s_data
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
-	t_img	img;
-}			t_data;
 
 typedef struct s_img
 {
@@ -34,5 +29,19 @@ typedef struct s_img
 	int		endian;
 }			t_img;
 
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	img;
+}			t_data;
 
+typedef struct s_rect
+{
+	int	x;
+	int	y;
+	int	width;
+	int	height;
+	int	color;
+}		t_rect;
 #endif
--- minilibx/reading_image.c
+++ minilibx/reading_image.c
@@ -1,6 +1,8 @@
 #include "minilibx.h"
 
-int		press_key(int key, t_data *data)
+#define BLUE_PIXEL 0x0000FF
+
+int	press_exit(int key, t_data *data)
 {
 	if (key == XK_Escape)
 	{
@@ -10,10 +12,62 @@ int		press_key(int key, t_data *data)
 	return (0);
 }
 
+int	click_close(int button, t_data *data)
+{
+	if (button == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		data->win_ptr = NULL;
+	}
+	return (0);
+}
+
+void	img_pix_put(t_img *img, int x, int y, int color)
+{
+	char *pixel;
+
+	pixel = img->addr + (y * img->line_lenght +  x * (img->bits_per_pixel / 8));
+	*(int *)pixel = color;
+}
+
+int	draw_rect(t_img *img, t_rect rect)
+{
+	int i;
+	int j;
+
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+		{
+			img_pix_put(img, j, i, rect.color);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	draw(t_data *data)
+{
+	t_rect rect;
+
+	rect.x = 0;
+	rect.y = 0;
+	rect.width = 500;
+	rect.height = 1080;
+	rect.color = BLUE_PIXEL;
+	if (data->win_ptr == NULL)
+		return (1);
+	draw_rect(&data->img, rect);
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.img, 0, 0);
+	return (0);
+}
+
 int     main(void)
 {
 	t_data	data;
-	t_img	img;
 
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
@@ -24,11 +78,13 @@ int     main(void)
 		free(data.win_ptr);
 		return (1);
 	}
-//	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
-//	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, img, 600, 400);
-//	mlx_loop_hook(data.mlx_ptr, press_key, (void *) 0);
-	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &press_key, &data);
+	data.img.img = mlx_new_image(data.mlx_ptr, 1920, 1080);
+	data.img.addr = mlx_get_data_addr(data.img.img, &data.img.bits_per_pixel, &data.img.line_lenght, &data.img.endian);
+	mlx_loop_hook(data.mlx_ptr, &draw, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &press_exit, &data);
+	mlx_hook(data.win_ptr, ButtonPress, ButtonPressMask, &click_close, &data);
 	mlx_loop(data.mlx_ptr);
+	mlx_destroy_image(data.mlx_ptr, data.img.img);
 	mlx_destroy_display(data.mlx_ptr);
 	free(data.mlx_ptr);
 }
