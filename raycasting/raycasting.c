ec9e9f13f323426c447681362d651686f41aef65
Sarah LESCURE <slescure@e2r3p20.42.fr>
2021-03-17 16:56:59 +0100

    continuation raycasting

--- raycasting/raycasting.c
+++ raycasting/raycasting.c
@@ -53,5 +53,9 @@ int raycasting(t_param *param)
 	ray.posy = param->perso.position_y;
 	initialisation_orientation(param, &ray);
 	print_ray(&ray);
+	while (ray.posx < param->map.max_length)
+	{
+		
+	}
 	return (0);
 }
